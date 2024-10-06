48fdb5bbf2b3ed9ac291c1449f387d1511c84555
Molly Carroll <spacemollusk.studio@gmail.com>
2024-10-06 14:31:56 +0200

    added star info script and made a few more adjustments for hand tracking settings

---
+++ Assets/Scripts/StarInfo.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class StarInfo : MonoBehaviour
+{
+    [SerializeField]
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
